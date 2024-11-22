1d10b6071f767a10e077cd75c603498317dd2ef0
Chirag Kaushik <chirag.k@osmosys.co>
2024-11-22 10:19:48 +0530

    push

---
+++ string.js
@@ -0,0 +1,41 @@
+const name = "chirag"
+const repoCount = 50
+
+//console.log(name + repoCount+ "value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+// const gameName = new String('ChiragCK')
+// console.log(gameName);
+// console.log(gameName.length)
+// console.log(gameName[2])
+// console.log(gameName._proto_);
+
+// console.log(gameName.toUpperCase);
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('c'));
+
+// const newString = gameName.substring(0,4);
+
+// console.log(newString);
+
+// const anotherString = gameName.slice(-6,4);
+// console.log(anotherString);
+
+const newStringOne = "   chirag"
+
+console.log(newStringOne);
+console.log(newStringOne.trim());
+//Read mdn doc for trim javascript function
+
+const url = "https://chirag.com/chirag%20Kaushik "
+//browser is space jo url me hai use convert kardega %20 jo ki hai url encoding
+
+url.replace('%20', '-');
+
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('Ram'));
+
+const wish = "happy birthday to you";
+console.log(wish.split(" "));
