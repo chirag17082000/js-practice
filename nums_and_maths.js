57db8284e6c022f0ffdbc938c334fd518ba1e986
Chirag Kaushik <chirag.k@osmosys.co>
2024-11-22 11:05:43 +0530

    math and num

---
+++ nums_and_maths.js
@@ -0,0 +1,20 @@
+// const score = 400
+// console.log(score)
+
+// const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 11123.8966
+
+// console.log(otherNumber.toPrecision(3));
+
+console.log(Math.random());
+console.log(Math.random()*10+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)))
