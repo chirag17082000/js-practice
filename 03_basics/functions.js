a770e3bf917646b4596837af0441ae864404f209
Chirag Kaushik <chirag.k@osmosys.co>
2024-12-02 10:45:20 +0530

    functions

--- 03_basics/functions.js
+++ 03_basics/functions.js
@@ -45,3 +45,38 @@ function loginUserMessage(username){ // default value bhi de sakte hai username 
 // console.log(loginUserMessage());
 
 
+// ... is called rest as well as spread operator based on its use
+
+// Rest operator is used when number of parameters in not clear
+function calculateCartPrice(...num1){
+
+    return num1
+}
+
+//console.log(calculateCartPrice(200, 300, 500));
+
+const user = {
+    username: "Chirag",
+    price: "199"
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}` );
+}
+
+//handleObject(user)
+// handleObject({
+//     username : "Ram",
+//     price: 500
+// })
+ 
+
+const myNewArray = [200, 300, 400];
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+// console.log(returnSecondValue(myNewArray));
+
+// console.log(returnSecondValue([200, 300, 400]));
+
