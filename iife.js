a25c9a50fe12565d3212fa1b900817d301b13c1c
Chirag Kaushik <chirag.k@osmosys.co>
2024-12-09 09:15:14 +0530

    iife

---
+++ iife.js
@@ -0,0 +1,25 @@
+// Immediately Invoked function Expressions (IIFE)
+
+// function chai(){
+//     console.log(`DB connected`);
+// }
+// chai()
+
+//iife function likhne ka tarika
+
+(function chai(){ //named iife
+    console.log(`DB connected`);
+})(); // di iife likhne ke liye isme semicolon lagana compulsory hai ek function ko end karna hoga warna ye error dega
+
+//Global scope ke pollution se problem hoti hai kayi bar usko hatane ke liye iife use karte hai
+// waha par jo declaration hoti hai usse bachne ke liye
+
+//FIRST () -> for function definition
+//SECOND () -> for function call/ execution
+
+//
+
+( (name) => {  //simple iife
+    console.log(`DB connected TWO ${name}`);
+} )('chirag')
+
