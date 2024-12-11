c0c3bbe8eb4439be84ce2108735057a85d78c315
Chirag Kaushik <chirag.k@osmosys.co>
2024-12-11 11:55:22 +0530

    switch

---
+++ control_flow/switch.js
@@ -0,0 +1,53 @@
+// switch (key) {
+//     case value : 
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3;
+//const month = "March"
+
+switch (month) {
+    case 1:  //case "January":
+        console.log("January")
+        break;
+    case 2: 
+        console.log("Feburary")
+        break;
+    case 3: 
+        console.log("March")
+        break;
+    case 4: 
+        console.log("April")
+        break;
+    case 5: 
+        console.log("May")
+        break;
+    case 6: 
+        console.log("June")
+        break;
+    case 7: 
+        console.log("July")
+        break;
+    case 8: 
+        console.log("Aug")
+        break;
+    case 9: 
+        console.log("Sept")
+        break;
+    case 10: 
+        console.log("Oct")
+        break;
+    case 11: 
+        console.log("Nov")
+        break;
+    case 12: 
+        console.log("Dec")
+        break;
+
+    default:
+        console.log("No match found")
+        break;
+}
