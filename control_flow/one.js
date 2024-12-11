b27f84ff581746954a68c215c52a732174aea15b
Chirag Kaushik <chirag.k@osmosys.co>
2024-12-11 11:48:02 +0530

    push

---
+++ control_flow/one.js
@@ -0,0 +1,72 @@
+// // if statement
+
+// if(2==2){
+//     console.log("yes equal");
+// }
+
+// const isUserLoggedIn = true;
+// if(isUserLoggedIn){
+//     console.log(`Yes user logged in ${isUserLoggedIn}`);
+// }
+
+// //<,>,<=,>=,==,!=,===
+
+// if(2 === "2"){
+//     console.log("executed");
+// }
+// else{
+//     console.log("Not executed")
+// }
+
+// const temperature = 41
+
+// if(temperature <50){
+//     console.log("Yes less than 50")
+// }
+// else{
+//     console.log(" not Less than 50") 
+// }
+
+// const score = 200
+
+// if(score > 100){
+//     // var power = "fly";   isme var use karenege toh ye scope ke bhar bhi use ho sakta hai but scope ke bhar use nhi hona chashiye toh let ya const use karenge
+//     const power = "fly";
+//     console.log(`User Power: ${power}`)
+// }
+// console.log(`User Power: ${power}`)
+
+//short hand notation for conditional operator
+
+const balance = 1000
+
+// if(balance > 500) console.log("test"); // implicit scope valid till single line
+    // console.log("test2");
+
+    // if(balance < 500){
+
+    //     console.log("Less than 500");
+    // } else if(balance < 750){
+    //     console.log("Less than 750");
+
+    // }else if(balance < 900){
+    //     console.log("Less than 900");
+
+    // }else{
+    //     console.log("Less than 1200");
+    // }
+
+    // const userLoggedIn = true;
+    // const debitCard = true;
+    // const loggedInFromGoogle = false;
+    // const loggedInFromEmail = true;
+
+    // if(userLoggedIn && debitCard){
+    //     console.log("Allow to buy course");
+    // }
+
+    // if(loggedInFromEmail || loggedInFromEmail){
+    //     console.log("User logged In");
+    // }
+
+    
