4688df69aa8df917add69b8f00dd4bc6b6779f8a
Chirag Kaushik <chirag.k@osmosys.co>
2024-12-16 11:38:20 +0530

    while

---
+++ iterations/two.js
@@ -0,0 +1,23 @@
+//while loop
+
+// let index =0
+// while(index <= 12 ){
+
+//     console.log(`value of index os ${index}`);
+//     index = index +2;
+// }
+
+// let myArray = ['flash', 'batman', 'superman'];
+
+// let arr =0;
+// while(arr < myArray.length){
+//     console.log(`value is ${myArray[arr]}`)
+//     arr++; 
+// }
+
+let score =11
+
+do {
+   console.log(`Score is ${score}`); 
+   score++;
+} while (score <= 10);
