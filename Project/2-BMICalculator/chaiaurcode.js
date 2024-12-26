41c84a46c1740d5a322efff7f005cf97fa18f21a
Chirag Kaushik <chirag.k@osmosys.co>
2024-12-26 17:04:13 +0530

    projects

---
+++ Project/2-BMICalculator/chaiaurcode.js
@@ -0,0 +1,24 @@
+const form = document.querySelector('form')
+// const height = parseInt(document.querySelector('#height').value);
+//This usecase will give you empty values
+// Jo koi bhi from hum submit karte hai wo GET ya POST request ki form me jata hai
+//To hume use yaha rokna hoga
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+    const results = document.querySelector('#results');
+    if(height === '' || height < 0 || isNaN(height)){
+
+        results.innerHTML = `Please give a valid height : ${height}`;
+    }else if(weight === '' || weight <0 || isNaN(weight)){
+        results.innerHTML = `Please give a valid weight ${weight}`;
+    }
+    else{
+        const bmi = (weight / ((height * height)/10000)).toFixed(2)
+        results.innerHTML = `<span>${bmi}</span>`;
+
+    }  
+});
