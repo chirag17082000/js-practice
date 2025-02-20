b2d3f6506f7d83b2a1aa46050864575a58a0d0eb
Chirag Kaushik <chirag.k@osmosys.co>
2025-02-20 10:49:01 +0530

    push

--- 01_basics/string.js
+++ 01_basics/string.js
@@ -1,14 +1,14 @@
 const name = "chirag"
+// console.log(name);
 const repoCount = 50
 
-//console.log(name + repoCount+ "value");
-
+// console.log(name + repoCount+ "value");
 // console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-// const gameName = new String('ChiragCK')
-// console.log(gameName);
-// console.log(gameName.length)
-// console.log(gameName[2])
+const gameName = new String('ChiragCK')
+console.log(gameName);
+console.log(gameName.length)
+console.log(gameName[2])
 // console.log(gameName._proto_);
 
 // console.log(gameName.toUpperCase);
@@ -22,20 +22,20 @@ const repoCount = 50
 // const anotherString = gameName.slice(-6,4);
 // console.log(anotherString);
 
-const newStringOne = "   chirag"
+// const newStringOne = "   chirag"
 
-console.log(newStringOne);
-console.log(newStringOne.trim());
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
 //Read mdn doc for trim javascript function
 
-const url = "https://chirag.com/chirag%20Kaushik "
+// const url = "https://chirag.com/chirag%20Kaushik "
 //browser is space jo url me hai use convert kardega %20 jo ki hai url encoding
 
-url.replace('%20', '-');
+// url.replace('%20', '-');
 
-console.log(url.replace('%20', '-'));
+// console.log(url.replace('%20', '-'));
 
-console.log(url.includes('Ram'));
+// console.log(url.includes('Ram'));
 
-const wish = "happy birthday to you";
-console.log(wish.split(" "));
+// const wish = "happy birthday to you";
+// console.log(wish.split(" "));
